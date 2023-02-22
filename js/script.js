a433d863439981342b28eec5c35070b1b233eb04
Łukasz Duszak <lukas94.WebDeveloper@gmail.com>
2023-02-22 20:42:12 +0100

    update header menu

--- js/script.js
+++ js/script.js
@@ -4,4 +4,11 @@ const menuNavigationElement = document.querySelector(".header__navigation");
 const showMenu = document.querySelector(".header__menu");
 showMenu.addEventListener("click", () => {
     menuNavigationElement.style.display = "block";
+    
+});
+
+const hideMenu = document.querySelector(".header__menu");
+
+hideMenu.addEventListener("onmouseout", () => { //zdarzenie onmouseleave też nie działa
+    menuNavigationElement.style.display = "none";
 });
